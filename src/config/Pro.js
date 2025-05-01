162b2a402fb26da56fd5250de8b436e65acb813b
Lê Thị Thuỷ Tiên <1204thuytien@gmail.com>
2025-05-02 00:23:37 +0700

    change name app

--- src/config/Pro.js
+++ src/config/Pro.js
@@ -41,115 +41,28 @@ export const SELSYSDICT0100 = "SELSYSDICT0100";
 //'http://117.2.160.69:91/hpdqapi/api/'; 39
 //'http://125.234.111.211/leadingstar_api/api/'; leadingstar
 
-export const APP_VERSION = "2.0.1";
+export const APP_VERSION = "1.0.0";
 
 export const ServerIP = {
-  tvs: "http://14.241.235.252:8081/api/",
-  // tvs: "http://14.241.235.252:8585/tvs_api/api/",
+  bst: "http://115.73.215.94:8082/api/api",
 };
 export const configAPI = [
   {
-    API_NAME: "http://113.161.148.227/bochang_api/api/",
-    CLIENT_ID: "BOCHANG",
-    CLIENT_KEY: "bochang@2021",
+    API_NAME: "http://115.73.215.94:8082/api/api/",
+    CLIENT_ID: "SYSHR",
+    CLIENT_KEY: "syshr@2025",
   },
-  {
-    API_NAME: "http://125.234.111.211/leadingstar_api/api/",
-    CLIENT_ID: "LS",
-    CLIENT_KEY: "ls@123",
-  },
-  {
-    API_NAME: "http://27.72.41.239:8080/miandoluong_api_v1/api/",
-    CLIENT_ID: "MIANDL",
-    CLIENT_KEY: "miandl@123",
-  },
-  {
-    API_NAME: "http://14.241.235.252:8484/tvs_api_v1/api/",
-    CLIENT_ID: "MIANDL201",
-    CLIENT_KEY: "miandl@123",
-  },
-  {
-    API_NAME: "http://27.72.41.239:8080/miandoluong_api_v1/api/",
-    CLIENT_ID: "MIANTK",
-    CLIENT_KEY: "miantk@123",
-  },
-  {
-    API_NAME: "http://apihr.hoaphatdungquat.vn/api/",
-    // API_NAME: "https://hr.hoaphatdungquat.vn/hpdq_api/",
-    CLIENT_ID: "HPDQ",
-    CLIENT_KEY: "hpdq@2021",
-  },
-  {
-    API_NAME: "http://117.2.160.99:92/hpdqapi/api/",
-    CLIENT_ID: "HPDQ40",
-    CLIENT_KEY: "hpdq@123",
-  },
-  {
-    API_NAME: "http://117.2.160.69:91/hoaphatdq_bus_api/api/",
-    CLIENT_ID: "HPDQ_TEST",
-    CLIENT_KEY: "hpdq@2023",
-  },
-  // {
-  //   API_NAME: "http://tinvietsoft.com/tvs_api/api/",
-  //   CLIENT_ID: "CSWIND",
-  //   CLIENT_KEY: "csw@2021",
-  // },
-  {
-    API_NAME: "http://14.241.235.252:8585/tvs_api/api/",
-    CLIENT_ID: "TVS",
-    CLIENT_KEY: "tvs@123",
-  },
-  {
-    API_NAME: "http://14.241.235.252:8081/api/",
-    CLIENT_ID: "TINVIET",
-    CLIENT_KEY: "tinviet@123",
-  },
-  {
-    API_NAME: "http://14.241.235.252:8585/tvs_api/api/",
-    CLIENT_ID: "HPDQ202",
-    CLIENT_KEY: "hpdq@123",
-  },
-  {
-    API_NAME: "http://113.160.172.6/miankimlien_api/api/",
-    CLIENT_ID: "MIANKL",
-    CLIENT_KEY: "miankl",
-  },
-  {
-    API_NAME: "http://14.241.235.252:8484/tvs_api_v1/api/",
-    CLIENT_ID: "TVSNEW",
-    CLIENT_KEY: "tvs@123",
-  },
-  // {
-  //   API_NAME: "http://tinvietsoft.com/tinviet_api_v1/api/",
-  //   CLIENT_ID: "ZINC",
-  //   CLIENT_KEY: "zinc@123",
-  // },
-  {
-    API_NAME: "http://117.2.160.69:91/hoaphatdq_api/api/",
-    CLIENT_ID: "HPDQ_NEW",
-    CLIENT_KEY: "hpdq@2022",
-  },
-  {
-    API_NAME: "http://113.161.148.227/bochang_api_v1/api/",
-    CLIENT_ID: "BOCHANG_V1",
-    CLIENT_KEY: "bochang@2022",
-  },
-  // {
-  //   API_NAME: "http://tinvietsoft.com/tinviet_api_v1/api/",
-  //   CLIENT_ID: "TINVIET_V1",
-  //   CLIENT_KEY: "tinviet@123",
-  // },
 ];
 
-export const ClientIdDefault = "TINVIET";
-export const buildFor = "tvs";
+export const ClientIdDefault = "BSTORE";
+export const buildFor = "bst";
 
 export const ipServer = () => {
   const url = AsyncStorage.getItem("API_URL");
-  let temp = ServerIP.tvs;
+  let temp = ServerIP.bst;
   console.log(" ************** url api_url ", url);
   if (url === null) {
-    AsyncStorage.setItem("API_URL", ServerIP.tvs);
+    AsyncStorage.setItem("API_URL", ServerIP.bst);
   } else {
     temp = url;
   }
